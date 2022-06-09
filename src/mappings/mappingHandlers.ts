1a499a9d643b1f75f30c700cb92044635a196d4b
shenger <714275326@qq.com>
2022-06-09 11:05:02 +0800

    fix

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -9,9 +9,30 @@ import { handleWithdrawn } from './handleWithdrawn'
 import { isNewAccount } from '../utils/isNewAccount'
 import { handleSlashed } from './handleSlashed'
 import { readDataFromFile } from '../utils/readDataFromFile'
+import { updateToken } from '../utils/updateToken'
+import { updateAccountBalance } from '../utils/updateAccountBalance'
 
 const nativeToken = getNativeCurrency(api as any);
 export const startHeight = 1100000;
+
+/*
+handle balances.Endowed
+An account was created with some free balance.
+Endowed { account: T::AccountId, free_balance: T::Balance }
+*/
+export async function handleBalancesEndowed(event: SubstrateEvent) {
+    const [account, free] = event.event.data
+    const accountId = account.toString()
+    const amount = BigInt(free.toString())
+    const tokenName = await getTokenName(nativeToken)
+    const blockNumber = event.block.block.header.number.toBigInt()
+    const isNew = isNewAccount(accountId, event);
+
+    await readDataFromFile(event)
+    if(blockNumber < BigInt(startHeight)) return;
+    await updateToken(tokenName, amount, amount, BigInt(0), BigInt(0), blockNumber, event.block.timestamp);
+    await updateAccountBalance(accountId, tokenName, amount, BigInt(0), BigInt(0), event.block.timestamp, blockNumber, isNew);
+}
 
 /*
 handle balances.Transfer
@@ -152,6 +173,53 @@ export async function handleBalancesReserveRepatriated(event: SubstrateEvent) {
     )
 }
 
+
+
+/*
+handle tokens.Endowed
+An account was created with some free balance.
+Endowed {
+	currency_id: T::CurrencyId,
+	who: T::AccountId,
+	amount: T::Balance,
+}
+*/
+export async function handleTokensEndowed(event: SubstrateEvent) {
+    const [currencyId, account, free] = event.event.data
+    const accountId = account.toString()
+    const amount = BigInt(free.toString())
+    const tokenName = await getTokenName(currencyId)
+    const blockNumber = event.block.block.header.number.toBigInt()
+    const isNew = isNewAccount(accountId, event);
+
+    await readDataFromFile(event)
+    if(blockNumber < BigInt(startHeight)) return;
+    await updateToken(tokenName, amount, amount, BigInt(0), BigInt(0), blockNumber, event.block.timestamp);
+    await updateAccountBalance(accountId, tokenName, amount, BigInt(0), BigInt(0), event.block.timestamp, blockNumber, isNew);
+}
+
+/*
+handle tokens.DustLost
+An account was removed whose balance was non-zero but below
+ExistentialDeposit, resulting in an outright loss.
+DustLost {
+	currency_id: T::CurrencyId,
+	who: T::AccountId,
+	amount: T::Balance,
+}
+*/
+export async function handleTokensDustLost(event: SubstrateEvent) {
+    const [currencyId, from, value] = event.event.data
+    const fromId = from.toString()
+    const amount = BigInt(value.toString())
+    const tokenName = await getTokenName(currencyId)
+    const blockNumber = event.block.block.header.number.toBigInt()
+
+    await readDataFromFile(event)
+    if(blockNumber < BigInt(startHeight)) return;
+    await handleWithdrawn(fromId, tokenName, amount, event.block.timestamp, blockNumber)
+}
+
 /**
 handle tokens.Transfer
 Transfer succeeded.
@@ -249,7 +317,7 @@ export async function handleTokensReserveRepatriated(event: SubstrateEvent) {
 }
 
 /*
-handle tokens.Deposite
+handle tokens.Deposited
 Deposited some balance into an account
 Deposited {
     currency_id: T::CurrencyId,
